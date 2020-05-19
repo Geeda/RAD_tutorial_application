4d6fa465c3cc5e60b6ebf5833da2a30f48f7257f
Will <willguida88@gmail.com>
2020-05-19 18:06:21 +1000

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtphost = '<your heroku app>.herokuapp.com'
+  config.action_mailer.delivery_method = :smtphost = 'https://sheltered-oasis-01857.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
